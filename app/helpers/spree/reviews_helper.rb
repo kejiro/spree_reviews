6593b553be00b9ce777f8cb4df3024cb6980141d
jimmy <jimmy@stenke.org>
2020-12-03 16:12:10 +0700

    fix helper if missing rating

--- app/helpers/spree/reviews_helper.rb
+++ app/helpers/spree/reviews_helper.rb
@@ -8,7 +8,8 @@ module Spree::ReviewsHelper
   end
 
   def txt_stars(n, show_out_of = true)
-    res = Spree.t(:star, count: n)
+    n = 0 if n.nil?
+    res = "#{Spree.t(:star, count: n)}"
     res += " #{Spree.t('out_of_5')}" if show_out_of
     res
   end
